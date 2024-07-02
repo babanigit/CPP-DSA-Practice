62754dd1b7f868d7e5476d455a42fdf433a95294
babanigit <Aniketgithub07>
2024-07-02 23:54:02 +0530

    added form linux

--- MMquestions/hw1_q2.cpp
+++ MMquestions/hw1_q2.cpp
@@ -1,18 +1,32 @@
 
 #include <iostream>
 #include <string>
+#include <sstream>
+
 using namespace std;
 
 // military to minutes
 int MTM(const string &timeData)
 {
-    // it will extract the first 2 substring
-    int hours = stoi(timeData.substr(0, 2)); // stoi = string to interger
-    // cout << hours << endl;
+    // // it will extract the first 2 substring
+    // int hours = stoi(timeData.substr(0, 2)); // stoi = string to interger
+    // // cout << hours << endl;
 
-    // it extract the two substring , excluding first 2
-    int minutes = stoi(timeData.substr(2, 2));
-    // cout << minutes << endl;
+    // // it extract the two substring , excluding first 2
+    // int minutes = stoi(timeData.substr(2, 2));
+    // // cout << minutes << endl;
+
+    // return hours * 60 + minutes;
+
+    stringstream ss(timeData.substr(0, 2));
+
+    int hours = 0;
+    ss >> hours;
+
+    ss.clear();
+    ss.str(timeData.substr(2, 2));
+    int minutes = 0;
+    ss >> minutes;
 
     return hours * 60 + minutes;
 }
@@ -34,7 +48,6 @@ int main()
     if (difference < 0)
         difference += 24 * 60; // add 24 hours to negative number
 
-    cout << " the difference is : " << difference << endl;
     // Calculate hours and minutes from total difference in minutes
     int hours = difference / 60; // it will give you the reminder number
     int mins = difference % 60;  // it will give you the the remaining number after divide
