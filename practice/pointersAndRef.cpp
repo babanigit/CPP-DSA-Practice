5fdbf05d43a0247459ee044a7adbc687b891909e
Aniket Panchal <aniketpanchal07@gmail.com>
2024-07-04 08:19:05 -0400

    added from win

--- practice/pointersAndRef.cpp
+++ practice/pointersAndRef.cpp
@@ -5,12 +5,26 @@
 using namespace std;
 
 int main(int argc, char const *argv[])
+
 {
     int x =10;
     
     int& ref =x;
-     cout << ref << endl;
-     ref =30;
-     cout << x << endl;
+    cout << ref << endl;
+    ref =30;
+    cout << x << endl;
+
+    int a=45,b=35;
+    cout << "before swap\n";
+    cout << "a= " << a << "b = "<<b << "\n";
+
+    int * address = &x;
+    cout << address << endl;
+
+    address = 3 ;
+
+        cout << x << endl;
+
     return 0;
+
 }
