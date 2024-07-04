2726a3b7934cb879600d3861eace1474dab996e5
Aniket Panchal <aniketpanchal07@gmail.com>
2024-07-04 03:11:21 -0400

    changes in file handling

--- practice/fileHandling.cpp
+++ practice/fileHandling.cpp
@@ -6,20 +6,24 @@ using namespace std;
 
 int main(int argc, char const *argv[])
 {
-    std::ifstream inputFile("input.txt");
-    std::ofstream outputFile("output.txt");
+    // std::
+    ifstream inputFile("txtfile/input.txt");
+    ofstream outputFile("txtfile/output.txt");
 
     if (!inputFile)
     {
-        cerr << "error opening input file " << endl;
+        cerr << "[error]: error opening input file " << endl;
         return 1;
     }
 
-    std::string line;
+    // std::
+    string line;
 
+    // while loop and get line will read each and every line of the file
     while (getline(inputFile, line))
     {
-        outputFile << line << endl;
+        cout << " the line is : " << line << endl;
+        outputFile << line << endl; // it will add line data lines and end with the endl
     }
 
     cout << "file copied successfully" << endl;
