6636969bcdbf12a0affbd9d2e7fa1db3d60bf064
Aniket Panchal <aniketpanchal07@gmail.com>
2024-07-05 05:25:05 -0400

    done with the hw2

--- MMquestions/hw2_q2.cpp
+++ MMquestions/hw2_q2.cpp
@@ -1,66 +1,25 @@
 
-
 #include <iostream>
 #include <string>
 using namespace std;
 
-int main()
+int main(int argc, char const *argv[])
 {
+    // total 12 * 9 alphabates including spaces
+    const string months = "January  February March    April    May      June     July     August   SeptemberOctober  November December ";
 
     cout << "Please enter an integer between 1 and 12: ";
     int monthNumber;
     cin >> monthNumber;
-    string monthName;
 
     if (monthNumber <= 12 && monthNumber >= 1)
     {
-        switch (monthNumber)
-        {
-        case 1:
-            monthName = "January";
-            break;
-        case 2:
-            monthName = "February";
-            break;
-        case 3:
-            monthName = "March";
-            break;
-        case 4:
-            monthName = "April";
-            break;
-        case 5:
-            monthName = "May";
-            break;
-        case 6:
-            monthName = "June";
-            break;
-        case 7:
-            monthName = "July";
-            break;
-        case 8:
-            monthName = "August";
-            break;
-        case 9:
-            monthName = "September";
-            break;
-        case 10:
-            monthName = "October";
-            break;
-        case 11:
-            monthName = "November";
-            break;
-        case 12:
-            monthName = "December";
-            break;
-        }
-        // default:
-        // break;
-
-        cout << " The corresponding month is : " << monthName << endl;
+        string month = months.substr((monthNumber - 1) * 9, 9); // 9 cause september has higest alphabates
+        cout << "The corresponding month is: " << month << endl;
     }
     else
     {
-        cout << " invalid! " << endl;
+        cout << "invalid!" << endl;
     }
 
     return 0;
---
+++ MMquestions/hw2_q3.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include <string>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    cout << "hello " << endl;
+
+    return 0;
+}
