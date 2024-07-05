ed4c84934673ef46a1db9dead51e7aab3aff1d6c
Aniket Panchal <aniketpanchal07@gmail.com>
2024-07-05 02:27:41 -0400

    done with q1

--- MMquestions/hw2_q1.cpp
+++ MMquestions/hw2_q1.cpp
@@ -1,23 +1,31 @@
 
 #include <iostream>
 #include <string>
+#include <sstream>
+
 using namespace std;
 
 int main(int argc, char const *argv[])
 {
 
+    // getting number
     cout << "Enter an integer between 1,000 and 999,999 including the comma: ";
     string inputWithComma;
     cin >> inputWithComma;
 
+    // finding comma postion
     int commaPostion = inputWithComma.rfind(',');
 
-    // cout << commaPostion << endl;
+    // removing comma
+    string numberWithoutComma = inputWithComma.substr(0, commaPostion) + inputWithComma.substr(commaPostion + 1);
 
-    string numberWithoutComma= inputWithComma.substr(0,commaPostion) + inputWithComma.substr(commaPostion+1);
+    // converting string into int
+    stringstream ss(numberWithoutComma);
+    int numbers;
+    ss >> numbers;
 
-    cout << "Actually, I don't like the comma. This is better: " << numberWithoutComma << endl;
+    // cout
+    cout << "Actually, I don't like the comma. This is better: " << numbers << endl;
 
     return 0;
-
 }
