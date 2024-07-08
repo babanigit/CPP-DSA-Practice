98a78840e6827c3088bb0b82fd6d0bf046e76212
babanigit <aniketpanchal07@gmail.com>
2024-07-08 10:42:53 +0530

    changed the spelling mistakes

--- MMquestions/hw2_q2.cpp
+++ MMquestions/hw2_q2.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 int main(int argc, char const *argv[])
 {
-    // total 12 * 9 alphabates including spaces
+    // total 12 * 9 alphabet including spaces
     const string months = "January  February March    April    May      June     July     August   SeptemberOctober  November December ";
 
     cout << "Please enter an integer between 1 and 12: ";
@@ -14,7 +14,7 @@ int main(int argc, char const *argv[])
 
     if (monthNumber <= 12 && monthNumber >= 1){
 
-        string month = months.substr((monthNumber - 1) * 9, 9); // 9 cause september has higest alphabates
+        string month = months.substr((monthNumber - 1) * 9, 9); // 9 cause september has higest alphabet
         cout << "The corresponding month is: " << month << endl;
     }else{
         
