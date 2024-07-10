c100a6a89be64380c2325b7dc74336b3ef7724ac
babanigit <aniketpanchal07@gmail.com>
2024-07-10 17:39:48 +0530

    addded

--- MMquestions/hw2_q3.cpp
+++ MMquestions/hw2_q3.cpp
@@ -6,14 +6,14 @@ using namespace std;
 int main()
 {
     cout << "I am here to help you solve quadratic equations." << endl;
-    // cout << "For me to be able to help you, you must format your equations in a specific way. In particular, writing-1s, 1s, and 0s is necessary." << endl;
-    // cout << "Here are examples of how to format your equations so that I understand..." << endl;
-    // cout << "1*x^2+0*x-1=0" << endl;
-    // cout << "1*x^2-1*x+0=0" << endl;
-    // cout << "1*x^2+1*x-6=0" << endl;
-    // cout << "1*x^2+2*x+1=0" << endl;
-    // cout << "-0.5*x^2-0.125*x+0.1875=0" << endl;
-    // cout << "0.1234*x^2+5.67*x+8.9=0" << endl;
+    cout << "For me to be able to help you, you must format your equations in a specific way. In particular, writing-1s, 1s, and 0s is necessary." << endl;
+    cout << "Here are examples of how to format your equations so that I understand..." << endl;
+    cout << "1*x^2+0*x-1=0" << endl;
+    cout << "1*x^2-1*x+0=0" << endl;
+    cout << "1*x^2+1*x-6=0" << endl;
+    cout << "1*x^2+2*x+1=0" << endl;
+    cout << "-0.5*x^2-0.125*x+0.1875=0" << endl;
+    cout << "0.1234*x^2+5.67*x+8.9=0" << endl;
     cout << "Now type your equation." << endl;
 
     string equation;
