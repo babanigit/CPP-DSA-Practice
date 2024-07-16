5ebb56b624a281eaf7612b6806ceb23b94238f31
babanigit <aniketpanchal07@gmail.com>
2024-07-16 10:42:31 +0530

    modified q3

--- MMquestions/hw3_q3.cpp
+++ MMquestions/hw3_q3.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <sstream>
 #include <vector>
-#include <limits>
+#include <limits> // min(), max()
 
 using namespace std;
 
@@ -10,31 +10,28 @@ int main()
     char choice;
     do
     {
-        cout << "Enter a list of ints with a single space between each adjacent pair of ints..." << endl;
         string input;
-        getline(cin, input); // Read entire line of input
-
-        cout << "the line of string is is : " << input << endl;
-
-        // Using stringstream to extract integers
-        stringstream ss(input);
-
-        cout << "after stringstream is : " << input << endl;
-
         vector<int> numbers;
         int num;
+        int maxNum = numeric_limits<int>::min();
+        int minNum = numeric_limits<int>::max();
+
+        cout << "Enter a list of ints with a single space between each adjacent pair of ints..." << endl;
+        getline(cin, input); // Read entire line of input
+
+        stringstream ss(input); // Using stringstream to extract integers
 
         // Read integers from stringstream
         while (ss >> num)
+        {
             numbers.push_back(num);
+            // cout << " the num is : " << num << endl;
+        }
 
         // Determine maximum and minimum
         if (!numbers.empty())
         {
-            int maxNum = numeric_limits<int>::min();
-            int minNum = numeric_limits<int>::max();
-
-            for (int number : numbers)
+            for (int number : numbers) // going through each vector
             {
                 if (number > maxNum)
                     maxNum = number;
