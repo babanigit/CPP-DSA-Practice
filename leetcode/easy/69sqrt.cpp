c415e3e6fea5061dcdb4445983ff36f3ccd62c27
aniket panchal <aniketpanchal07@gmail.com>
2024-08-13 21:44:06 +0530

    added on leetcode easy q

---
+++ leetcode/easy/69sqrt.cpp
@@ -0,0 +1,51 @@
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Solution
+{
+public:
+    int mySqrt(int x)
+    {
+
+        if (x < 2)
+            return x;
+
+        int low = 0;
+        int high = x;
+        int mid;
+        long long mid_squared; // Use long long to avoid overflow
+
+        while (low <= high)
+        {
+            mid = low + (high - low) / 2;
+            mid_squared = static_cast<long long>(mid) * mid;
+
+            if (mid_squared == x)
+            {
+                return mid;
+            }
+            else if (mid_squared < x)
+            {
+                low = mid + 1;
+            }
+            else
+            {
+                high = mid - 1;
+            }
+            /* code */
+        }
+
+        return high;
+    }
+};
+
+int main(int argc, char const *argv[])
+{
+    Solution sol;
+    int data = sol.mySqrt(4);
+    cout << "the output is : " << data << endl;
+
+    return 0;
+}
---
+++ practice/print_size.cpp
@@ -0,0 +1,17 @@
+
+
+#include <iostream>
+
+int main() {
+    std::cout << "Size of char: " << sizeof(char) << " byte(s)" << std::endl;
+    std::cout << "Size of short: " << sizeof(short) << " byte(s)" << std::endl;
+    std::cout << "Size of int: " << sizeof(int) << " byte(s)" << std::endl;
+    std::cout << "Size of long: " << sizeof(long) << " byte(s)" << std::endl;
+    std::cout << "Size of long long: " << sizeof(long long) << " byte(s)" << std::endl;
+    std::cout << "Size of float: " << sizeof(float) << " byte(s)" << std::endl;
+    std::cout << "Size of double: " << sizeof(double) << " byte(s)" << std::endl;
+    std::cout << "Size of long double: " << sizeof(long double) << " byte(s)" << std::endl;
+    std::cout << "Size of bool: " << sizeof(bool) << " byte(s)" << std::endl;
+    std::cout << "Size of wchar_t: " << sizeof(wchar_t) << " byte(s)" << std::endl;
+    return 0;
+}
