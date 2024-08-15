2c672ff20f9fa469014a233b4226c3e6006b5d36
aniket panchal <aniketpanchal07@gmail.com>
2024-08-15 16:56:24 +0530

    added

--- leetcode/easy/118_Pascals_Triangle.cpp
+++ leetcode/easy/118_Pascals_Triangle.cpp
@@ -1,24 +1,30 @@
-
 #include <iostream>
 #include <vector>
-#include <string>
 using namespace std;
 
 class Solution
 {
-private:
-    /* data */
 public:
-    // Solution(/* args */);
-    // ~Solution();
-    void fun()
+    void printVector(const vector<vector<int>> &vec)
     {
-        cout << "hello geneate for class solution " << endl;
+        cout << "printing the vector --- "  <<endl;
+        for (const auto &row : vec)
+        {
+            for (int val : row)
+            {
+                cout << val << " ";
+            }
+            cout << endl;
+        }
     }
 
     vector<vector<int>> generate(int numRows)
     {
         vector<vector<int>> prevRows;
+
+        // Print initial state
+        cout << "Initial prevRows:" << endl;
+        printVector(prevRows);
 
         if (numRows == 0)
             return {};
@@ -27,8 +33,12 @@ public:
 
         prevRows = generate(numRows - 1);
 
+        // Print state after recursion
+        cout << "post prevRows:" << endl;
+        printVector(prevRows);
+
         vector<int> newRow(numRows, 1); // Initialize with 'numRows' size, filled with 1
-        
+
         for (int i = 1; i < numRows - 1; i++)
         {
             newRow[i] = prevRows.back()[i - 1] + prevRows.back()[i];
@@ -36,17 +46,23 @@ public:
 
         prevRows.push_back(newRow);
 
+        // Print state after adding the new row
+        cout << "Final prevRows:" << endl;
+        printVector(prevRows);
+
         return prevRows;
     }
 };
 
 int main(int argc, char const *argv[])
 {
-
     Solution Sol;
 
-   int numRows = 5; // Example number of rows
+    int numRows = 5; // Example number of rows
     vector<vector<int>> result = Sol.generate(numRows);
-    /* code */
+
+    cout << "Generated Pascal's Triangle:" << endl;
+    Sol.printVector(result);
+
     return 0;
 }
